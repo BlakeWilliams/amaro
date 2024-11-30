b6ee912d65d03f04052e579c22245ab4b93426bf
Blake Williams <blake@blakewilliams.me>
2024-11-30 14:55:03 -0500

    Clean up session+flash middleware implementations

--- httprouter/middleware/session/flash/integration_test.go
+++ httprouter/middleware/session/flash/integration_test.go
@@ -1,8 +1,12 @@
 package flash_test
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/blakewilliams/amaro/httprouter"
 	"github.com/blakewilliams/amaro/httprouter/middleware/session"
 	"github.com/blakewilliams/amaro/httprouter/middleware/session/flash"
 	"github.com/stretchr/testify/require"
@@ -12,23 +16,59 @@ type sessionData struct {
 	Flash *flash.Messages
 }
 
+type requestContext struct {
+	sessionData *sessionData
+	httprouter.RequestContext
+}
+
+func (r *requestContext) Flash() *flash.Messages {
+	return r.sessionData.Flash
+}
+
+func (r *requestContext) SessionData() *sessionData {
+	return r.sessionData
+}
+
+func (r *requestContext) SetSessionData(sd *sessionData) {
+	r.sessionData = sd
+}
+
 func TestFlash(t *testing.T) {
 	store := session.New("testing", session.NewVerifier("iiiiiiiiiiiiiiii"), nil, func() *sessionData {
-		return &sessionData{}
+		return &sessionData{
+			Flash: &flash.Messages{},
+		}
+	})
+	router := httprouter.New(func(rctx httprouter.RequestContext) *requestContext {
+		return &requestContext{
+			RequestContext: rctx,
+		}
 	})
 
-	data := &sessionData{
-		Flash: &flash.Messages{},
-	}
+	router.Use(session.Middleware[*requestContext](store))
+	router.Use(flash.Middleware)
 
-	data.Flash.Set("success", "You did it!")
-	require.Equal(t, "", data.Flash.Get("success"))
+	router.Get("/", func(ctx context.Context, rctx *requestContext) {
+		rctx.Flash().Set("foo", "hello!")
+	})
 
-	// Convert to cookie, then extract
-	cookie, err := store.ToCookie(data)
-	require.NoError(t, err)
-	newData, err := store.FromCookie(cookie)
-	require.NoError(t, err)
+	router.Get("/flash", func(ctx context.Context, rctx *requestContext) {
+		rctx.Response().Write(
+			[]byte(rctx.Flash().Get("foo")),
+		)
+	})
 
-	require.Equal(t, "You did it!", newData.Flash.Get("success"))
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	res := httptest.NewRecorder()
+	router.ServeHTTP(res, req)
+
+	cookies := res.Header().Get("Set-Cookie")
+	require.NotEmpty(t, cookies)
+
+	req = httptest.NewRequest(http.MethodGet, "/flash", nil)
+	req.Header.Set("Cookie", cookies)
+	res = httptest.NewRecorder()
+	router.ServeHTTP(res, req)
+
+	require.Equal(t, "hello!", res.Body.String())
 }
